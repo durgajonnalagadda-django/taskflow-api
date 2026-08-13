c19f971b6210df5d53a9d7338334b60b88f8bc92
JONNALAGADDA NAGA VENKATA KANAKA DURGA <durgajonnalagadda20@gmail.com>
2026-08-13 14:32:51 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,11 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./taskflow.db"
+# IMPORTANT: Change this to your MySQL username, password, db name
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:your_password@localhost/taskflow_db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+# This function will be used in main.py
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
